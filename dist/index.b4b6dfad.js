6b33e714d5c4ab47de591ccbc72fec268f8069de
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-01-10 07:15:20 -0800

    commented out extra movie info
    all this info will be pulled from an API later on

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27271,43 +27271,43 @@ const MainView = ()=>{
             id: 1,
             title: "Avatar",
             image: "https://www.imdb.com/title/tt0499549/mediaviewer/rm2864126209/?ref_=tt_ov_i",
-            director: {
-                name: "James Cameron",
-                bio: "This is Jim's bio",
-                birth_date: "08-16-1954"
-            },
-            genre: {
-                name: "Sci-Fi",
-                description: "this is sci-fi"
-            }
+            director: "James Cameron" /*{
+        name: "James Cameron",
+        bio: "This is Jim's bio",
+        birth_date: "08-16-1954"
+      },
+      genre: {
+        name: "Sci-Fi",
+        description: "this is sci-fi"
+      }*/ 
         },
         {
             id: 2,
             title: "The Lord of the Rings",
             image: "https://www.imdb.com/title/tt0167261/mediaviewer/rm306845440/?ref_=tt_ov_i",
-            director: {
-                name: "Peter Jackson",
-                bio: "This is Peter's bio",
-                birth_date: "10-31-1961"
-            },
-            genre: {
-                name: "Fantasy",
-                description: "this is fantasy"
-            }
+            director: "Peter Jackson" /*{
+        name: "Peter Jackson",
+        bio: "This is Peter's bio",
+        birth_date: "10-31-1961"
+      },
+      genre: {
+        name: "Fantasy",
+        description: "this is fantasy"
+      }*/ 
         },
         {
             id: 3,
             title: "Star Wars",
             image: "https://www.imdb.com/title/tt0076759/mediaviewer/rm164871937/?ref_=tt_ov_i",
-            director: {
-                name: "George Lucas",
-                bio: "This is George's bio",
-                birth_date: "05-14-1944"
-            },
-            genre: {
-                name: "Sci-Fi",
-                description: "this is sci-fi"
-            }
+            director: "George Lucas" /*{
+        name: "George Lucas",
+        bio: "This is George's bio",
+        birth_date: "05-14-1944"
+      },
+      genre: {
+        name: "Sci-Fi",
+        description: "this is sci-fi"
+      }*/ 
         }
     ]);
     const [selectedMovie, setSelectedMoive] = (0, _react.useState)(null);
@@ -27328,7 +27328,7 @@ const MainView = ()=>{
     }, undefined);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: movies.map((movie)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieCard.MovieCard), {
-                movieData: movie,
+                movie: movie,
                 onMovieClick: (newSelectedMovie)=>{
                     setSelectedMoive(newSelectedMovie);
                 }
@@ -27343,7 +27343,7 @@ const MainView = ()=>{
         columnNumber: 5
     }, undefined);
 };
-_s(MainView, "8yq0+lov0GuheCCPaSpgrbMV8/Y=");
+_s(MainView, "yyByillUsW025w38EdjGBgt1D88=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,7 +8,7 @@ export const MainView = () => {
       id: 1,
       title: "Avatar",
       image: "https://www.imdb.com/title/tt0499549/mediaviewer/rm2864126209/?ref_=tt_ov_i",
-      director: {
+      director: "James Cameron" /*{
         name: "James Cameron",
         bio: "This is Jim's bio",
         birth_date: "08-16-1954"
@@ -16,13 +16,13 @@ export const MainView = () => {
       genre: {
         name: "Sci-Fi",
         description: "this is sci-fi"
-      }
+      }*/
     },
     {
       id: 2,
       title: "The Lord of the Rings",
       image: "https://www.imdb.com/title/tt0167261/mediaviewer/rm306845440/?ref_=tt_ov_i",
-      director: {
+      director: "Peter Jackson" /*{
         name: "Peter Jackson",
         bio: "This is Peter's bio",
         birth_date: "10-31-1961"
@@ -30,13 +30,13 @@ export const MainView = () => {
       genre: {
         name: "Fantasy",
         description: "this is fantasy"
-      }
+      }*/
     },
     {
       id: 3,
       title: "Star Wars",
       image: "https://www.imdb.com/title/tt0076759/mediaviewer/rm164871937/?ref_=tt_ov_i",
-      director: {
+      director: "George Lucas" /*{
         name: "George Lucas",
         bio: "This is George's bio",
         birth_date: "05-14-1944"
@@ -44,7 +44,7 @@ export const MainView = () => {
       genre: {
         name: "Sci-Fi",
         description: "this is sci-fi"
-      }
+      }*/
     }
   ]);
 
@@ -65,7 +65,7 @@ export const MainView = () => {
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
-          movieData={movie}
+          movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMoive(newSelectedMovie);
           }}
