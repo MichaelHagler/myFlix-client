fcbfd64caa03971b13e2e0ffbfc161252838cd9c
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-01-31 06:34:11 -0800

    movie-card refactored for react-bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,33 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
+    <Card
+      className="h-100"
+      sytle={{ cursor: "pointer" }}
       onClick={() => {
         onMovieClick(movie);
       }}
+    >
+      <Button
+        onClick={() => {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }}
       >
-      {movie.Title}
-    </div>
+        Logout
+      </Button>
+      
+      <Card.Img varitant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Card.Text>{movie.Genre}</Card.Text>
+        <Card.Text>{movie.Director}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
@@ -18,14 +37,14 @@ MovieCard.PropTypes = {
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
+      Description: PropTypes.string.isRequired,
     }),
     Director: PropTypes.PropTypes.shape({
       Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired
+      Bio: PropTypes.string.isRequired,
     }),
     ImagePath: PropTypes.string.isRequired,
     // Featured: PropTypes.boolean.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
